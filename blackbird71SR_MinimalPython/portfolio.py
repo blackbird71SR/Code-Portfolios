1c94f565cda5936568893665eb52bb3a83d8a0db
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2019-10-01 00:30:34 +0530

    Add python portfolio

---
+++ blackbird71SR_MinimalPython/portfolio.py
@@ -0,0 +1,8 @@
+name = 'Omkar Ajnadkar'
+title = 'FINAL YEAR CSE UNDERGRADUATE - DATA SCIENCE ENTHUSIAST'
+skills = ['Machine/Deep Learning', 'Web Development']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
