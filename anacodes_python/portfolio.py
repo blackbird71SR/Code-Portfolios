57d0b52e1831701ff3801266a2a1e4554eec5f6b
anacodes <anaghaajnadkar@gmail.com>
2019-10-01 01:48:42 +0530

    add python portfolio

---
+++ anacodes_python/portfolio.py
@@ -0,0 +1,8 @@
+name = 'Anagha'
+title = 'SECOND YEAR CSE UNDERGRADUATE - DATA SCIENCE ENTHUSIAST'
+skills = ['Deep Learning', 'Web Development']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
