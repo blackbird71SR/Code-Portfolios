36d2e1adb4c6b4a1c5c9e83e9da1b7b6b592c72b
Dharm7900 <51825672+Dharm7900@users.noreply.github.com>
2019-10-01 10:10:10 -0700

    added my detail
    added name,title,skills

--- blackbird71SR_MinimalPython/portfolio.py
+++ blackbird71SR_MinimalPython/portfolio.py
@@ -1,8 +1,8 @@
-name = 'Omkar Ajnadkar'
-title = 'FINAL YEAR CSE UNDERGRADUATE - DATA SCIENCE ENTHUSIAST'
-skills = ['Machine/Deep Learning', 'Web Development']
-print(name)
-print(title)
-print('Skills:')
-for i in skills:
-  print(i)
+name = 'Dharmendra kumar'
+title = 'finance'
+skills = ['python', 'Web Development']
+if print(name)
+  if print(title)
+    if print('Skills:')
+       for i in skills:
+          print(i)
