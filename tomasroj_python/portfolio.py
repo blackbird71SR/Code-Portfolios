6ce6ceb6df3c6e945e2377e3f5727e651c3913b7
Tomas Roj <rojtomas@email.cz>
2019-10-01 11:35:34 +0200

    Added my portfolio 🎉

---
+++ tomasroj_python/portfolio.py
@@ -0,0 +1,8 @@
+name = "Tomáš Roj"
+username = "@TomasRoj"
+about_self = "I am 14 years old fan of code and open-source"
+print(name)
+print("MY GitHub username: " + username + " :Dont worry to follow :-D")
+
+for i in range(5):
+    print(about_self)
