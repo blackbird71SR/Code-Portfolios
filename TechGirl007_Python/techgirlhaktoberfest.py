aafc5686149843f26a8053c5a94ce6dabca938f3
TechGirl007 <chippinginnz@gmail.com>
2019-10-06 19:45:08 +0800

    Added Doc with Python

---
+++ TechGirl007_Python/techgirlhaktoberfest.py
@@ -0,0 +1,8 @@
+name = 'TechGirl007'
+title = 'Cyber Security and Law Student'
+skills = ['Coding', 'Web Development', 'Legal Research']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
