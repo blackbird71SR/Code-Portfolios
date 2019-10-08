988cce178a22db12b500a437b2922951c66ee8b0
Priyadasrhan singh <singhpd75@gmail.com>
2019-10-08 19:52:20 +0530

    first commit

---
+++ pd_minimalPython/pdPython.py
@@ -0,0 +1,9 @@
+name = 'Priyadarshan Singh'
+title = '3rd year CSE undergraduate'
+skills = ['FullStack Development', 'Deep Learning']
+print(name)
+print(title)
+print('skills:')
+for i in skills:
+    print(i)
+print('The Emperor Protects')
