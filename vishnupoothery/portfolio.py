7829b6e41907a013eacac60ac1570f0d3aa2f745
Vishnu Poothery <vishnupoothery@gmail.com>
2019-10-11 10:43:26 +0530

    add vishnu portfolio

---
+++ vishnupoothery/portfolio.py
@@ -0,0 +1,8 @@
+name = 'Vishnu Pothery'
+title = 'Hobbyist'
+skills = ['App Development', 'Web Development','Data Science']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
