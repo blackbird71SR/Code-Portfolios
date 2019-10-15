077e4f6e9593b819bf23d02aed6c41e273d70826
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2019-10-15 21:02:39 +0530

    Adding Portfolio 🎉

---
+++ Saurabh-FullStackDev_PY/my-portfolio.py
@@ -0,0 +1,8 @@
+name = 'Saurabh Gupta'
+title = 'Full  Stack Developer'
+skills = ['Web Development', 'Advance Javascript', 'React', 'Node JS', 'Mongo DB', 'python']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
