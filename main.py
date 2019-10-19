7e413d9beaae7f8c32b22ebcca144933c495b803
Sanath Singavarapu <sanathsingavarapu99@gmail.com>
2019-10-20 02:53:28 +0530

    Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+name = 'Sanath Singavarapu'
+title = 'SkillSet'
+skills = ['App Development', 'Web Development','Data Science','Machine Learning','Deep Learning']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
