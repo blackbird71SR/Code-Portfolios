9b281f43a5d5d5709bfaa6a56227b76458e368a5
Sanath <30856736+Killer2499@users.noreply.github.com>
2019-10-22 14:34:08 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-name = 'Sanath Singavarapu'
-title = 'SkillSet'
-skills = ['App Development', 'Web Development','Data Science','Machine Learning','Deep Learning']
-print(name)
-print(title)
-print('Skills:')
-for i in skills:
-  print(i)
