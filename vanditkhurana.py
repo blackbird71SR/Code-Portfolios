494c056b79cce383042836fd29c586344932551a
Vandit <52314194+vanditkhurana@users.noreply.github.com>
2019-10-26 15:06:44 +0530

    Create vanditkhurana.py
    Here's a quick glimpse of mine.

---
+++ vanditkhurana.py
@@ -0,0 +1,12 @@
+name="Vandit"
+print(name)
+branch="CSE"
+print(branch)
+Skills=['Python', 'C Programming', 'HTML']
+print("Skills : ")
+for skill in Skills:
+  print(skill)
+Hobbies=['Watching American TV Shows', 'Playing Guitar', 'Singing']
+for hobby in Hobbies:
+  print(hobby)
+print("-------Thank You-------")
