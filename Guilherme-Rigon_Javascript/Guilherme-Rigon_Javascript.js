a70d30831e1865af94825f88621a61fb11a361b8
Guilherme Luiz <guilherme.rigon1988@gmail.com>
2019-10-29 12:57:30 -0300

    'portifolio_js'

---
+++ Guilherme-Rigon_Javascript/Guilherme-Rigon_Javascript.js
@@ -0,0 +1,11 @@
+var Nome = 'Guilherme Rigon';
+var Titulo = 'Graduando em Sistemas de Informação';
+var Habilidades = ['Desenvolvimento Web', 'Desenvolvimento Mobile', 'Desenvolvimento para Desktop'];
+
+console.log(Nome);
+console.log(Titulo);
+console.log('Habilidades:');
+
+Habilidades.forEach((x) => {
+	console.log(x);
+})
