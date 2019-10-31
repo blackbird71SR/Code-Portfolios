e8f652cd05ab89cd4d7126609243792a84282185
clwarnock@gmail.com <lee_warnock@shi.com>
2019-10-31 11:36:08 -0500

    add lee warnock python portfolio

---
+++ lee-warnock-python-portfolio/leeWarnock_Portfolio.py
@@ -0,0 +1,8 @@
+name = 'Lee Warnock'
+title = 'Full-Time AWS Consultant - Part-Time Coder'
+skills = ['AWS Solutions Architecture', 'Web Development']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+  print(i)
