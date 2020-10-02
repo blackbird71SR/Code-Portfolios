f78cda10bf44ebedaae17c26694597f654eddacc
Mehul Gupta <70238140+MehulGupta21@users.noreply.github.com>
2020-10-02 23:04:25 +0530

    This is Mehul Gupta, It's my Portfolio.

---
+++ MehulGupta_portfolio/MehulGupta_portfolio.py
@@ -0,0 +1,8 @@
+name = 'Mehul Gupta'
+title = 'Undergraduate student from India'
+skills = ['Machine/Deep Learning', 'Web Development', 'Python']
+print(name)
+print(title)
+print('Skills:')
+for i in skills:
+    print(i)
