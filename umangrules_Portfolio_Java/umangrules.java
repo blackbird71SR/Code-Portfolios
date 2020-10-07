1f0e8a91c422a4597f3954604f7ef31e34fff77f
Umang Seth <umangrules@gmail.com>
2020-10-07 22:00:20 +0530

    My portfolio in Java
    Added my portfolio in Java

---
+++ umangrules_Portfolio_Java/umangrules.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class umangrules
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    System.out.println();
+		
+		System.out.println("                           Umang Seth");
+		System.out.println();
+		System.out.println("                CSE Graduate 2020 from AIMT, Lucknow ");
+		System.out.println();
+		System.out.println("                   SKILLS");
+		System.out.println();
+		System.out.println("     - DSA");
+		System.out.println("     - Web Devlopment");
+		System.out.println();
+		System.out.println("                   INTERETS");
+		System.out.println();
+		System.out.println("     - Cricket");
+		System.out.println("     - Football");
+		System.out.println("     - Gaming");
+		System.out.println("     - Travelling");
+		System.out.println();
+		System.out.println("                   LANGUAGES KNOWN");
+		System.out.println();
+		System.out.println("     - English");
+		System.out.println("     - Hindi");
+		
+	}
+}
