73284eac515d174e936ff3ca3064ebb780aa9ad6
BhawanaMishra <43779190+BhawanaMishra@users.noreply.github.com>
2020-10-09 17:03:08 +0530

    Create portfolio_bm.py
    My simple and decent portfolio in Python

---
+++ portfolio_bm.py
@@ -0,0 +1,11 @@
+print("*****PORTFOLIO IN PYTHON*****")
+name = 'Bhawana Mishra'
+yr = "3rd Year"
+hobby = ["basketball","sketch"]
+techskill = ["Web dev", "Intermediate coding"]
+print("Name: " + name)
+print("Year: " + yr)
+print("My Hobbies : Playing "+ hobby[0] + " and I also like to " + hobby[1])
+print("My Skills:")
+for i in techskill:
+      print("* "+i)
