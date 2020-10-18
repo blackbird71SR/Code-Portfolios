afe573b9dbdb51555ff0642f0b8b264b6e08ea20
Venji10 <bennisteinir@gmail.com>
2020-10-18 12:45:43 +0200

    create venji10's portfolio in C

---
+++ venji10_C/portfolio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("*******************************************************\n");
+    printf("			Venji10\n");
+    printf("		Likes pizza, code and GNU/linux\n");
+    printf("		    Develops in Java and C\n");
+    printf("*******************************************************\n");
+
+
+    return 0;
+}
