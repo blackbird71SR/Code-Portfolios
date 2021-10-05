f602aadef3e6f3e7c3f3e19dc43920fe38d4f2c1
priyanshuarora1 <priyanshu.18bcs1097@abes.ac.in>
2021-10-05 20:53:14 +0530

    Add Resume

---
+++ priyanshu/portfolio.py
@@ -0,0 +1,11 @@
+name = 'Priyanshu Arora'
+title = 'Intern Develop at Medly!'
+skills = ['Web Development','Django','Python','C','SQL']
+print(name)
+print()
+print(title)
+print()
+print('Skills:')
+print()
+for skill in skills:
+  print('*',skill)
