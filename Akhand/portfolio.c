ed4cf2d8cca3bbb6344a3cfbe99add7f2c4a0f29
Akhand Agrawal <83289867+AkhandAgrawal@users.noreply.github.com>
2021-10-30 04:49:39 +0530

    portfolio in c

---
+++ Akhand/portfolio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("***********************************************\n\n");
+    printf("\t\tAKHAND AGRAWAL\n");
+    printf(" In 2nd Year B.TECH CSE at SRM University\n");
+    printf(" Learnt Basic C, C++, Python and HTML\n");
+    printf(" Hobbies - Playing Guitar, Competitive Coding\n\n");
+    printf("***********************************************\n");
+
+    return 0;
+}
